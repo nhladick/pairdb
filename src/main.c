9a6ef0e31bab50fa244d4ca960880dc99407caa9
Nikolai Hladick <nhladick@gmail.com>
2025-08-04 23:02:32 -0400

    Adds simple loop logic in main

--- src/main.c
+++ src/main.c
@@ -5,14 +5,19 @@
 #include "parse.h"
 #include "memorydb.h"
 
+#define INBUFF_SIZE 256
+
 int main(int argc, char *argv[])
 {
     parse_data prs_data = init_parse_data();
+    char inbuff[INBUFF_SIZE];
 
     bool run_loop = true;
 
     while (run_loop) {
         printf("keydb>> ");
+        fgets(inbuff, INBUFF_SIZE, stdin);
+        run_loop = false;
     }
 
     destroy_parse_data(prs_data);
