a1bab1d8e0da83ddaa51fa64d07d751a5ceedee7
Nikolai Hladick <nhladick@gmail.com>
2025-08-12 22:56:31 -0400

    Removes function test stubs from db modules

--- src/memorydb.c
+++ src/memorydb.c
@@ -2,9 +2,7 @@
 #include "fileio.h"
 #include "hashtable.h"
 
-char *db_stub()
-{
-    fileio_stub();
-    hash_stub();
-    return "from db stub";
+struct db_object {
+    // hash table handle
+    //
 }
--- src/memorydb.h
+++ src/memorydb.h
@@ -1,6 +1,6 @@
 #ifndef MEMORYDB_H
 #define MEMORYDB_H
 
-char *db_stub();
+
 
 #endif // MEMORYDB_H
