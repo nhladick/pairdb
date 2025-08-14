94b52ca9692479d0d9fc7ffc6bf08ad7ff70d40c
Nikolai Hladick <nhladick@gmail.com>
2025-08-13 23:38:27 -0400

    Adds initial hash table structs and functions

--- src/hashtable.c
+++ src/hashtable.c
@@ -1,8 +1,70 @@
-#include <stdio.h>
+#include <stdlib.h>
 
 #include "hashtable.h"
 
-void hash_stub()
+#define HVAL_INIT 0x811c9dc5
+#define FNV_PRIME 0x01000193
+
+struct node {
+    char *key;
+    char *val;
+    struct node *next;
+};
+
+struct hashtbl_obj {
+    struct node **arr;
+    size_t arrsize;
+};
+
+hashtbl init_hashtbl(size_t tblsize)
 {
-    printf("in hash\n");
+    hashtbl ptr = malloc(sizeof(struct hashtbl_obj));
+    if (!ptr) {
+        return NULL;
+    }
+
+    ptr->arr = malloc(sizeof(struct node *) * tblsize);
+    if (!ptr->arr) {
+        free(ptr);
+        return NULL;
+    }
+
+    ptr->arrsize = tblsize;
+
+    return ptr;
 }
+
+size_t get_tbl_size(hashtbl ht)
+{
+    return ht->arrsize;
+}
+
+
+static unsigned int fnv_hash(void *p_in)
+{
+    unsigned int hval = HVAL_INIT;
+    unsigned char *p = (unsigned char *) p_in;
+
+    while (*p) {
+        hval ^= (unsigned int) *p++;
+        hval *= FNV_PRIME;
+    }
+
+    return hval;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/hashtable.h
+++ src/hashtable.h
@@ -1,6 +1,12 @@
 #ifndef HASHTABLE_H
 #define HASHTABLE_H
 
-void hash_stub();
+#include <stddef.h>
+
+typedef struct hashtbl_obj *hashtbl;
+
+hashtbl init_hashtbl(size_t tblsize);
+
+size_t get_tbl_size(hashtbl ht);
 
 #endif // HASHTABLE_H
