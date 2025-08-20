d040f4121830458382303cf28b5af29f82d2069a
Nikolai Hladick <nhladick@gmail.com>
2025-08-19 23:59:38 -0400

    Adds test directory and script to download Unity framework files

---
+++ test/download_unity.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+wget https://raw.githubusercontent.com/ThrowTheSwitch/Unity/refs/heads/master/src/unity.c
+wget https://raw.githubusercontent.com/ThrowTheSwitch/Unity/refs/heads/master/src/unity.h
+wget https://raw.githubusercontent.com/ThrowTheSwitch/Unity/refs/heads/master/src/unity_internals.h
