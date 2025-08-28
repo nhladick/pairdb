fd355dc1da68a6a56e7c41fb22c2ae1eadf03b22
Nikolai Hladick <nhladick@gmail.com>
2025-08-28 10:36:23 -0400

    Allocates and destroys hashtbl in init and destroy functions, implements db add

--- src/memorydb.c
+++ src/memorydb.c
@@ -5,6 +5,8 @@
 #include "hashtable.h"
 #include "keydbstring.h"
 
+#define INIT_TBL_SIZE 32
+
 struct db_object {
     hashtbl tbl;
     char *tblname;
@@ -14,6 +16,12 @@ db_obj init_db_obj()
 {
     db_obj ptr = calloc(1, sizeof(struct db_object));
     if (!ptr) {
+        return NULL;
+    }
+
+    ptr->tbl = init_hashtbl(INIT_TBL_SIZE);
+    if (!ptr->tbl) {
+        free(ptr);
         return NULL;
     }
 
@@ -28,6 +36,18 @@ db_obj init_db_obj()
 
 void destroy_db_obj(db_obj dbo)
 {
+    destroy_hashtbl(dbo->tbl);
     free(dbo->tblname);
     free(dbo);
+}
+
+// Input: two strings, key and val, to be added to database.
+// Output: -1 if key already exists,
+//         -2 on memory allocation failure,
+//          1 on success.
+// Attempt to add key that already exists results in failure.
+int add(db_obj dbo, char *key, char *val)
+{
+    int result = put(dbo->tbl, key, val);
+    return result;
 }
--- src/memorydb.h
+++ src/memorydb.h
@@ -6,4 +6,11 @@ typedef struct db_object *db_obj;
 db_obj init_db_obj();
 void destroy_db_obj(db_obj dbo);
 
+// Input: two strings, key and val, to be added to database.
+// Output: -1 if key already exists,
+//         -2 on memory allocation failure,
+//          1 on success.
+// Attempt to add key that already exists results in failure.
+int add(db_obj dbo, char *key, char *val);
+
 #endif // MEMORYDB_H
