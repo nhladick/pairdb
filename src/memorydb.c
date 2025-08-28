1ca5d419702701bf09d688ac5e235ce0b4d7e3ae
Nikolai Hladick <nhladick@gmail.com>
2025-08-28 10:23:21 -0400

    Adds db object and init and destroy functions

--- src/memorydb.c
+++ src/memorydb.c
@@ -1,9 +1,33 @@
+#include <stdlib.h>
+
 #include "memorydb.h"
 #include "fileio.h"
 #include "hashtable.h"
 #include "keydbstring.h"
 
 struct db_object {
-    // hash table handle
-    //
+    hashtbl tbl;
+    char *tblname;
 };
+
+db_obj init_db_obj()
+{
+    db_obj ptr = calloc(1, sizeof(struct db_object));
+    if (!ptr) {
+        return NULL;
+    }
+
+    ptr->tblname = calloc(1, TBL_NAME_MAX);
+    if (!ptr->tblname) {
+        free(ptr);
+        return NULL;
+    }
+
+    return ptr;
+}
+
+void destroy_db_obj(db_obj dbo)
+{
+    free(dbo->tblname);
+    free(dbo);
+}
--- src/memorydb.h
+++ src/memorydb.h
@@ -1,6 +1,9 @@
 #ifndef MEMORYDB_H
 #define MEMORYDB_H
 
+typedef struct db_object *db_obj;
 
+db_obj init_db_obj();
+void destroy_db_obj(db_obj dbo);
 
 #endif // MEMORYDB_H
