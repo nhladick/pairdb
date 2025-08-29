bf58eb3cf008e544bcffee4493c0734a0571e654
Nikolai Hladick <nhladick@gmail.com>
2025-08-28 21:02:38 -0400

    Implements db_manager init function

--- src/db_manager.c
+++ src/db_manager.c
@@ -1,11 +1,79 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "db_manager.h"
 #include "hashtable.h"
 #include "memorydb.h"
+#include "keydbstring.h"
 #include "fileio.h"
+
+static const char *TBL_LIST_FNAME = "../data/tbl_list";
+static const size_t INIT_HASHTBL_SIZE = 32;
 
 struct db_manager {
     hashtbl active_tbls;
 
 };
+
+/*
+ *
+ ******** Start - static / internal functions ********
+ *
+ *
+ */
+
+// Read table list file from disk.
+//      file format:
+//          line 1: int file_length
+//          line 2...line n: file names, one per line
+// Populate hashtable with file name strings.
+// Returns 1 on success.
+// Returns -1 on failure.
+int load_tbl_list(const char *fname, hashtbl tbl)
+{
+    FILE *inf = fopen(fname, "r");
+    if (!inf) {
+        return -1;
+    }
+
+    char buff[TBL_NAME_MAX];
+
+    // Get length of file list
+    // from first line of file
+    fgets(buff, TBL_NAME_MAX, inf);
+    int len = atoi(buff);
+
+    for (int i = 0; i < len; i++) {
+        fgets(buff, TBL_NAME_MAX, inf);
+        // Leave val field empty
+        put(tbl, buff, "");
+    }
+
+    fclose(inf);
+    return 1;
+}
+
+/*
+ *
+ ******** End - static / internal functions ********
+ *
+ *
+ */
+
+db_mgr init_db_mgr()
+{
+    db_mgr ptr = calloc(1, sizeof(struct db_manager));
+    if (!ptr) {
+        return NULL;
+    }
+
+    ptr->active_tbls = init_hashtbl(INIT_HASHTBL_SIZE);
+    if (!ptr->active_tbls) {
+        free(ptr);
+        return NULL;
+    }
+
+    load_tbl_list(TBL_LIST_FNAME, ptr->active_tbls);
+
+    return ptr;
+}
--- src/db_manager.h
+++ src/db_manager.h
@@ -1,6 +1,8 @@
 #ifndef DB_MANAGER_H
 #define DB_MANAGER_H
 
+typedef struct db_manager *db_mgr;
 
+db_mgr init_db_mgr();
 
 #endif // DB_MANAGER_H
