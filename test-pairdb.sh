1bb581b86f6d94f7b95160ffbbf72c7348c5808b
Nikolai Hladick <nhladick@gmail.com>
2025-09-02 16:28:07 -0400

    Updates keydb names to pairdb

--- test-pairdb.sh
+++ test-pairdb.sh
@@ -13,17 +13,17 @@ fi
 gcc -o test/unity/unity -c test/unity/unity.c
 
 # Run parse tests
-gcc -o test/test_parse test/test_parse.c test/unity/unity src/parse.c src/keydbstring.c
+gcc -o test/test_parse test/test_parse.c test/unity/unity src/parse.c src/pairdbstring.c
 echo "---------- Parse Tests ------------" > test/test_output.txt
 ./test/test_parse >> test/test_output.txt
 
 # Run hashtable tests
 gcc -o test/test_hashtable test/test_hashtable.c test/unity/unity src/hashtable.c \
-                                                            src/keydbstring.c
+                                                            src/pairdbstring.c
 echo "--------- Hashtable Tests ---------" >> test/test_output.txt
 ./test/test_hashtable >> test/test_output.txt
 
 # Hashtable memory allocation/deallocation test
-gcc -o test/test_mem_hashtable test/test_mem_hashtable.c src/hashtable.c src/keydbstring.c
+gcc -o test/test_mem_hashtable test/test_mem_hashtable.c src/hashtable.c src/pairdbstring.c
 echo "---------- Hashtable Memory Test ------------" >> test/test_output.txt
 valgrind ./test/test_mem_hashtable 2>> test/test_output.txt
