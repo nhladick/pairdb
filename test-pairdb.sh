0e225c8b3ea0fdb323708399d7998e0da114230f
Nikolai Hladick <nhladick@gmail.com>
2025-09-03 12:26:18 -0400

    Updates test script to use new stringutil name

--- test-pairdb.sh
+++ test-pairdb.sh
@@ -15,24 +15,24 @@ gcc -o test/unity/unity -c test/unity/unity.c
 mkdir -p test/build/
 
 # Run string tests
-gcc -o test/build/test_pairdbstring test/test_pairdbstring.c test/unity/unity \
-                                        src/pairdbstring.c
+gcc -o test/build/test_stringutil test/test_stringutil.c test/unity/unity \
+                                        src/stringutil.c
 echo "---------- String Tests -----------" > test/test_output.txt
-./test/build/test_pairdbstring >> test/test_output.txt
+./test/build/test_stringutil >> test/test_output.txt
 
 # Run parse tests
-gcc -o test/build/test_parse test/test_parse.c test/unity/unity src/parse.c src/pairdbstring.c
+gcc -o test/build/test_parse test/test_parse.c test/unity/unity src/parse.c src/stringutil.c
 echo "---------- Parse Tests ------------" >> test/test_output.txt
 ./test/build/test_parse >> test/test_output.txt
 
 # Run hashtable tests
 gcc -o test/build/test_hashtable test/test_hashtable.c test/unity/unity src/hashtable.c \
-                                                            src/pairdbstring.c
+                                                            src/stringutil.c
 echo "--------- Hashtable Tests ---------" >> test/test_output.txt
 ./test/build/test_hashtable >> test/test_output.txt
 
 # Hashtable memory allocation/deallocation test
-gcc -o test/build/test_mem_hashtable test/test_mem_hashtable.c src/hashtable.c src/pairdbstring.c
+gcc -o test/build/test_mem_hashtable test/test_mem_hashtable.c src/hashtable.c src/stringutil.c
 echo "---------- Hashtable Memory Test ------------" >> test/test_output.txt
 valgrind ./test/build/test_mem_hashtable 2>> test/test_output.txt
 
