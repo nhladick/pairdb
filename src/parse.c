707f8c85e9c7bd52f765ff57004a5018e00112be
Nikolai Hladick <nhladick@gmail.com>
2025-09-03 22:23:25 -0400

    Updates comments/documentation, updates input to tokenize function

--- src/parse.c
+++ src/parse.c
@@ -1,3 +1,49 @@
+/*
+ * --------------------------------------------------
+ * Copyright (C) 2025 Nikolai Hladick
+ * SPDX-License-Identifier: MIT
+ * https://github.com/nhladick/pairdb
+ * nhladick@gmail.com
+ * --------------------------------------------------
+ *
+ * The parse_input function reads a string buffer and returns
+ * a parse object. The parse object has a CMD and three
+ * string values. If the first argument is a valid command and
+ * the correct number of arguments is provided for that
+ * command, the CMD field is set to the proper command value,
+ * and the key, value, and table name fields are set accordingly.
+ * If a command is not recognized or an incorrect number of
+ * arguments is provided for a command, then the CMD field
+ * is set to FAIL.
+ *
+ * Input is parsed in 4 steps:
+ *
+ *  1. Preprocess -     The newline character left in the input
+ *                      buffer by fgets is replaced with the NUL
+ *                      character. The input buffer is scanned.
+ *                      All spaces outside of sections enclosed
+ *                      in single or double quotation marks are
+ *                      replaced with a marker character for
+ *                      further processing.
+ *
+ *  2. Tokenize -       The preprocessed input buffer is broken
+ *                      into separate strings based on the new
+ *                      marker character and quotation mark
+ *                      characters.
+ *
+ *  3. Command Parse -  The initial string is parsed to determine
+ *                      whether it is a valid command. If it is
+ *                      not valid, the enum CMD element of the
+ *                      parse object is set to FAIL.
+ *
+ *  4. Argument Parse - The rest of the strings are parsed. The
+ *                      argument parse function determines whether
+ *                      the correct number of arguments are present
+ *                      for each command. Extra arguments are ignored.
+ *
+ */
+
+
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
@@ -10,16 +56,15 @@ enum {
     MAX_ARGS = 5
 };
 
-/*
- *
- ********** start - static/internal functions **********
- *
- */
+/*---------- start - static/internal functions ------------*/
 
 static void preprocess(char *cp)
 {
-    // remove newline char from fgets
-    // end points to null char
+    // fgets leaves newline char in
+    // buffer as last char of string.
+    // Replace newline char with NUL
+    // char to get NUL terminated
+    // input string.
     char *end = &cp[strlen(cp) - 1];
     *end = '\0';
 
@@ -44,13 +89,13 @@ static void preprocess(char *cp)
     }
 }
 
-static void tokenize(char *inbuff, char *argv[])
+static void tokenize(char *inbuff, char *argv[], size_t len)
 {
     char *delim = "\t\'\"";
     char *temp = strtok(inbuff, delim);
     argv[0] = temp;
 
-    for (size_t i = 1; i < MAX_ARGS && temp; i++) {
+    for (size_t i = 1; i < len && temp; i++) {
         temp = strtok(NULL, delim);
         argv[i] = temp;
     }
@@ -143,17 +188,13 @@ static void parse_args(char *argv[], struct parse_object *prs_data)
     }
 }
 
-/*
- *
- ********** end - static/internal functions **********
- *
- */
+/*-------------- end - static/internal functions --------------*/
 
 void parse_input(char *inbuff, struct parse_object *prs_data)
 {
     preprocess(inbuff);
     char *argv[MAX_ARGS] = {NULL};
-    tokenize(inbuff, argv);
+    tokenize(inbuff, argv, MAX_ARGS);
     prs_data->cmd = parse_cmd(argv[0]);
     parse_args(argv, prs_data);
 }
--- src/parse.h
+++ src/parse.h
@@ -1,3 +1,24 @@
+/*
+ * --------------------------------------------------
+ * Copyright (C) 2025 Nikolai Hladick
+ * SPDX-License-Identifier: MIT
+ * https://github.com/nhladick/pairdb
+ * nhladick@gmail.com
+ * --------------------------------------------------
+ *
+ * The parse_input function reads a string buffer and returns
+ * a parse object. The parse object has a CMD and three
+ * string values. If the first argument is a valid command and
+ * the correct number of arguments is provided for that
+ * command, the CMD field is set to the proper command value,
+ * and the key, value, and table name fields are set accordingly.
+ * If a command is not recognized or an incorrect number of
+ * arguments is provided for a command, then the CMD field
+ * is set to FAIL.
+ *
+ */
+
+
 #ifndef PARSE_H
 #define PARSE_H
 
